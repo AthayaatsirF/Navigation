7c3b7a7eee03f78071bcd7e3e14facc1214e7fd3
manggoweek <67376397+AthayaatsirF@users.noreply.github.com>
2020-06-30 15:58:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const trigger = document.querySelector('.trigger');
 const nav = document.querySelector('.full-screen-nav');
 const backdrop = document.querySelector('.backdrop');
 
-trigger .addEventListener('click', () => nav.classList.add('open-nav'));
-trigger .addEventListener('click', () => nav.classList.remove('open-nav'));
+trigger.addEventListener('click',function(){ nav.classList.add('open-nav')};
+trigger.addEventListener('click',function(){ nav.classList.add('open-nav')};
